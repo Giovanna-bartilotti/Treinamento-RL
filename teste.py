dd352ce2df0b5cfc9c32268987c18a703564fbe9
Giovanna Relvas Bartilotti <grbartilotti@gmail.com>
2020-12-03 21:56:54 -0300

    Primeiro arquivo do projeto

---
+++ teste.py
@@ -0,0 +1,8 @@
+﻿import os
+
+if __name__ == '__main__':
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    from src.logger import setup_logger
+    setup_logger('main.log')
+    import manager
+    manager.start()
